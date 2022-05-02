2ea6079ab383763d7c5fb968d0a729a680c82f39
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-05-02 16:08:42 -0700

    create Node class with initialize and inlcuded comparable method

---
+++ lib/node.rb
@@ -0,0 +1,20 @@
+class Node
+
+  include Comparable
+
+  attr_accessor :data, :left_child, :right_child
+
+  def initialize(data, left_child=nil, right_child=nil)
+    @data = data
+    @left_child = left_child
+    @right_child = right_child
+  end
+
+  # include comparable module and compare nodes using their data attribute
+  def <=>(other)
+    data.size <=> other.data.size
+  end
+end
+
+n1 = Node.new(4)
+n2 = Node.new(4)
---
+++ lib/tree.rb
@@ -0,0 +1,8 @@
+class Tree
+  def initialize(arr)
+    @arr = arr
+  end
+
+  def build_tree(arr)
+  end
+end
