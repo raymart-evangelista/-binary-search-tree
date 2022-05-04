51e44bd7ff9d2fe91c41ad1bbfff7d355fb111a6
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-05-03 20:26:20 -0700

    change Tree#insert from iteration to recursion

--- lib/tree.rb
+++ lib/tree.rb
@@ -33,38 +33,25 @@ class Tree
     return node
   end
 
-  def insert(value)
-    # do not insert anything if tree already has value
-    # compare value to root, recurse down 
-    new_node = Node.new(value)
-    curr_node = @root
-
-    # is 0 less than or greater than 5? less than -> go into left child
-    # is 0 less than or greater than 2? less than -> go into left child
-    # is 0 less than or greater than 1? less than -> go into left child if !nil else 1.child = value
-    until new_node == curr_node || curr_node.left_child.nil? || curr_node.right_child.nil? do
-      # traverse down the tree
-      if new_node < curr_node
-        curr_node = curr_node.left_child
-      elsif new_node > curr_node
-        curr_node = curr_node.right_child
-      end
-
-      if !curr_node.left_child.nil? && new_node == curr_node.left_child
-        puts "node already exists bro"
-        return
-      elsif !curr_node.right_child.nil? && new_node == curr_node.right_child
-        puts "node already exists man"
-        return
-      end
+  def insert(value, node=@root)
+    # base case
+    if value == node.data
+      return nil
     end
 
-    if new_node < curr_node
-      curr_node.left_child = new_node
-    elsif new_node > curr_node
-      curr_node.right_child = new_node
-    elsif new_node == curr_node
-      puts "node already exists"
+    # recursive case
+    if value < node.data
+      if node.left_child.nil?
+        node.left_child = Node.new(value)
+      else
+        insert(value, node.left_child)
+      end
+    elsif value > node.data
+      if node.right_child.nil?
+        node.right_child = Node.new(value)
+      else
+        insert(value, node.right_child)
+      end
     end
   end
   
@@ -182,6 +169,7 @@ class Tree
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
     pretty_print(node.left_child, "#{prefix}#{is_left ? '    ' : '│   '}", true) if node.left_child
   end
+  
 end
 
 arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
@@ -189,6 +177,8 @@ arr = [1,2,3,4,5,6,7,8,9]
 arr = [50, 30, 70, 20, 40, 60, 80]
 tree = Tree.new(arr, 0, arr.length-1)
 
-tree.delete(20)
-tree.delete(30)
-tree.delete(50)
+tree.insert(100)
+tree.pretty_print
+# tree.delete(20)
+# tree.delete(30)
+# tree.delete(50)
