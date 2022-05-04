d3fff7145131f1740a5e4b93bafa79ca7deda3f3
raymart-evangelista <raymart.s.evangelista@gmail.com>
2022-05-04 14:52:23 -0700

    create working Tree#level_order, however, didn't test with block given yet

--- lib/tree.rb
+++ lib/tree.rb
@@ -121,6 +121,33 @@ class Tree
     end
   end
 
+  def level_order(&block)
+    # arr to be used as queue
+    node = @root
+    queue = []
+    ordered = []
+    # if tree is empty, return nil
+    if node.nil?
+      return nil
+    else
+    # traverse tree in breadth-first level order
+      queue.push(node)
+      until queue.empty? do
+        curr_node = queue[0]
+        ordered.push(curr_node.data)
+        queue.push(curr_node.left_child) if !curr_node.left_child.nil?
+        queue.push(curr_node.right_child) if !curr_node.right_child.nil?
+        # remove node form queue once finished
+        #yield each node to the provided block
+        block.call(queue[0]) if block_given?
+        queue.shift
+      end
+    end
+    return ordered
+    # return array of values if no block given
+
+  end
+
   def pretty_print(node = @root, prefix = '', is_left = true)
     pretty_print(node.right_child, "#{prefix}#{is_left ? '│   ' : '    '}", false) if node.right_child
     puts "#{prefix}#{is_left ? '└── ' : '┌── '}#{node.data}"
@@ -129,25 +156,25 @@ class Tree
   
 end
 
-arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
-arr = [1,2,3,4,5,6,7,8,9]
-arr = [50, 30, 70, 20, 40, 60, 80]
+# arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+# arr = [1,2,3,4,5,6,7,8,9]
+# arr = [50, 30, 70, 20, 40, 60, 80]
+arr = %w[a b c d e f g h i j k]
 tree = Tree.new(arr, 0, arr.length-1)
+p tree.level_order
 
-tree.insert(100)
-tree.insert(200)
-tree.insert(400)
-tree.pretty_print
-tree.delete(400)
-tree.pretty_print
-tree.delete(200)
-tree.pretty_print
-tree.delete(100)
-tree.pretty_print
-tree.delete(70)
-tree.pretty_print
-# tree.delete(20)
-# tree.delete(30)
-tree.delete(50)
-tree.pretty_print
-p tree.find(100)
+# tree.insert(100)
+# tree.insert(200)
+# tree.insert(400)
+# tree.pretty_print
+# tree.delete(400)
+# tree.pretty_print
+# tree.delete(200)
+# tree.pretty_print
+# tree.delete(100)
+# tree.pretty_print
+# tree.delete(70)
+# tree.pretty_print
+# tree.delete(50)
+# tree.pretty_print
+# p tree.find(100)
